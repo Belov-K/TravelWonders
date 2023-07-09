eab9911a78686eed588defecb928cca8bd7b75ab
kiril <kaz1nak3@mail.ru>
2023-07-09 23:27:02 +0300

    feat(tours): добовил модели

--- TravelWonders/apps/tours/admin.py
+++ TravelWonders/apps/tours/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 
-from .models import Tour
+from .models import Hotel, Tour, City
 
+admin.site.register(Hotel)
 admin.site.register(Tour)
+admin.site.register(City)
--- TravelWonders/apps/tours/models.py
+++ TravelWonders/apps/tours/models.py
@@ -1,17 +1,43 @@
 from django.db import models
 
-class Tour(models.Model):
+class Hotel(models.Model):
     image=models.ImageField('картинка')
     name=models.CharField('название отеля', max_length=50)
     country=models.CharField('страна', max_length=50)
     city=models.CharField('город', max_length=50)
     price=models.CharField('цена', max_length=50)
     star=models.FloatField('звезды')
-    tour=models.FloatField('сколько туров')
+    tourr=models.FloatField('сколько туров')
+
+    class Meta:
+        verbose_name="Отель"
+        verbose_name_plural="Отели"
+
+    def __str__(self):
+        return self.name
+
+class Tour(models.Model):
+
+    hotel=models.ForeignKey(Hotel, on_delete=models.CASCADE)
+
+    on=models.CharField('туда', max_length=50)
+    out=models.CharField('обратно', max_length=50)
+    name=models.CharField('название тура', max_length=50)
+    price=models.IntegerField('цена')
 
     class Meta:
         verbose_name="Тур"
         verbose_name_plural="Туры"
 
+class City(models.Model):
+    image=models.ImageField('картинка')
+    name_city=models.CharField('название города', max_length=50)
+    price=models.IntegerField('цена')
+    country=models.CharField('город', max_length=50)
+
+    class Meta:
+        verbose_name="Город"
+        verbose_name_plural="Города"
+
     def __str__(self):
-        return self.name
+        return self.name_city
