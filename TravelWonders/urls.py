5ec6b573cfb6a9bb571f499e2805c3f30eee4acd
kiril <kaz1nak3@mail.ru>
2023-07-09 21:35:28 +0300

    feat(tours): добавил вывод текста

--- TravelWonders/urls.py
+++ TravelWonders/urls.py
@@ -18,6 +18,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('names/', include('names.urls')),
+    path('', include('tours.urls')),
     path('admin/', admin.site.urls),
 ]
