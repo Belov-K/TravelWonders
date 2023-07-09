632460241d58650169b24c7df39c1157b579d800
kiril <kaz1nak3@mail.ru>
2023-07-09 22:09:38 +0300

    feat(tours): добавил вывод текста

--- TravelWonders/apps/tours/models.py
+++ TravelWonders/apps/tours/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+class Tour(models.Model):
+    image=models.ImageField('картинка')
+    name=models.CharField('название отеля', max_length=50)
+    country=models.CharField('страна', max_length=50)
+    city=models.CharField('город', max_length=50)
+    price=models.CharField('цена', max_length=50)
+    star=models.FloatField('звезды')
+    tour=models.FloatField('сколько туров')
+
+    class Meta:
+        verbose_name="Тур"
+        verbose_name_plural="Туры"
+
+    def __str__(self):
+        return self.name
